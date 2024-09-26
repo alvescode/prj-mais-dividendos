5ab2af64347668fa6f5aebcac4613ec3b40dc8ec
Rafaella Calocini <rafa.calocini@gmail.com>
2024-09-26 10:01:15 -0300

    front-end messages implentação

--- Desenvolvimento/3.Implementacao/front-end/mais-dividendos-dashboard/src/components/NavMessage.jsx
+++ Desenvolvimento/3.Implementacao/front-end/mais-dividendos-dashboard/src/components/NavMessage.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 function NavMessage() {
   return (
-    <div>NavMessage</div>
-  )
+    <li className="nav-item dropdown">
+      <button className="nav-link nav-icon" data-bs-toggle="dropdown">
+        <i className="bi bi-chat left-text"></i>
+        <span className="badge bg-success badge-number">3</span>
+      </button>
+      <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+        <li className="dropdown-header">
+          Você tem 3 novas mensagens
+          <button className="badge rounded-pill bg-primary p-2 ms-2">Ver tudo</button>
+        </li>
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+        <li className="message-item">
+          <button className="message-link">
+            <img src="../images/homem.png" alt="Avatar de Elio Zacarias" className="rounded-circle" />
+            <div>
+              <h4>Elio Zacarias</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur provident ex natus aperiam consequuntur vero.</p>
+              <p>4 horas atrás</p>
+            </div>
+          </button>
+        </li>
+      </ul>
+    </li>
+  );
 }
 
-export default NavMessage
+export default NavMessage;
