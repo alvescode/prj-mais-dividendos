fa282023ddebcae9aac92308c58005a576811f1b
Rafaella Calocini <rafa.calocini@gmail.com>
2024-09-27 21:44:52 -0300

    sideBar styling

--- Desenvolvimento/3.Implementacao/front-end/mais-dividendos-dashboard/src/components/Nav/NavMessage.jsx
+++ Desenvolvimento/3.Implementacao/front-end/mais-dividendos-dashboard/src/components/Nav/NavMessage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function NavMessage() {
   return (
     <li className="nav-item dropdown">
-      <button className="nav-link nav-icon" data-bs-toggle="dropdown">
+      <a href='#' className="nav-link nav-icon" data-bs-toggle="dropdown">
         <i className="bi bi-chat left-text"></i>
         <span className="badge bg-success badge-number">3</span>
-      </button>
+      </a>
       <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
         <li className="dropdown-header">
           Você tem 3 novas mensagens
@@ -16,14 +16,14 @@ function NavMessage() {
           <hr className="dropdown-divider" />
         </li>
         <li className="message-item">
-          <button className="message-link">
+          <a href='#' className="message-link">
             <img src="../images/homem.png" alt="Avatar de Elio Zacarias" className="rounded-circle" />
             <div>
               <h4>Elio Zacarias</h4>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur provident ex natus aperiam consequuntur vero.</p>
               <p>4 horas atrás</p>
             </div>
-          </button>
+          </a>
         </li>
       </ul>
     </li>
